90e6512d314aee1b64966f5ca36ea17b493536fd
Reggie Dev <ReggieD3V@Reggies-MacBook-Air.local>
2025-10-10 09:22:17 -0400

    Remove blog link from public navigation

--- personal-website/src/components/layout/navigation.tsx
+++ personal-website/src/components/layout/navigation.tsx
@@ -11,7 +11,7 @@ const navItems = [
   { name: "Home", href: "/" },
   { name: "About", href: "/#about" },
   { name: "Projects", href: "/#projects" },
-  { name: "Blog", href: "/blog" },
+  // { name: "Blog", href: "/blog" },
   { name: "Contact", href: "/#contact" },
 ]
 
